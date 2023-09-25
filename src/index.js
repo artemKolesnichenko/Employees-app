d3c2d7a2d8f98d21219a2aed3e8b9b3cd8e06f42
Artem Kolesnichenko <kolesnichenko.2001@gmail.com>
2023-09-25 15:00:44 +0300

    layout finished

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import App from './components/app/app'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h2>Hello Wrold!</h2>
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
 
 
